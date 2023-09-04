f2adb8f7e843b52644642dff56912eeca73feadb
Udith_Dev_Wee <daveudith@gmail.com>
2023-09-05 01:10:03 +0530

    update put method

--- 06_JSON_Processing/src/CustomerServlet.java
+++ 06_JSON_Processing/src/CustomerServlet.java
@@ -1,6 +1,4 @@
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -120,7 +118,7 @@ public class CustomerServlet extends HttpServlet {
                 objectBuilder.add("message", "Successfully Deleted");
                 objectBuilder.add("status", 200);
                 writer.print(objectBuilder.build());
-            } else{
+            } else {
                 JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
                 objectBuilder.add("data", "");
                 objectBuilder.add("message", "Wrong Id Entered");
@@ -143,12 +141,19 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Update");
-        String cusId = req.getParameter("customerId");
-        String cusName = req.getParameter("customerName");
-        String cusAddress = req.getParameter("customerAddress");
-        String cusSalary = req.getParameter("customerSalary");
+
+        //we have to updated data from Json format
+
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        String cusId = jsonObject.getString("id");
+        String cusName = jsonObject.getString("name");
+        String cusAddress = jsonObject.getString("address");
+        String cusSalary = jsonObject.getString("salary");
 
         System.out.println(cusId + " " + cusName + " " + cusAddress + " " + cusSalary);
+
+        PrintWriter writer = resp.getWriter();
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_system", "root", "1234");
@@ -160,16 +165,38 @@ public class CustomerServlet extends HttpServlet {
 
             resp.setContentType("application/json");
 
-            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+
             if (preparedStatement.executeUpdate() > 0) {
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
                 objectBuilder.add("data", objectBuilder.build());
-                objectBuilder.add("Status", 200);
-                objectBuilder.add("message", "Updated");
-                PrintWriter writer = resp.getWriter();
+                objectBuilder.add("status", 200);
+                objectBuilder.add("message", "Updated Successful");
+
+                writer.print(objectBuilder.build());
+            } else {
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("data", objectBuilder.build());
+                objectBuilder.add("status", 400);
+                objectBuilder.add("message", "Updated Unsuccessful");
+
                 writer.print(objectBuilder.build());
             }
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
+
+            resp.setStatus(200);
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("data", e.getLocalizedMessage());
+            objectBuilder.add("message", "Error");
+            objectBuilder.add("status", 500);
+            writer.print(objectBuilder.build());
+
+        } catch (ClassNotFoundException th) {
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("data", th.getLocalizedMessage());
+            objectBuilder.add("message", "Update Fail");
+            objectBuilder.add("status", 500);
+            writer.print(objectBuilder.build());
         }
     }
 }
