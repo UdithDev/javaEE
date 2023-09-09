e56459fdfacacaaeee2737dc9d3820df0a7dcf16
Udith_Dev_Wee <daveudith@gmail.com>
2023-09-09 12:01:22 +0530

    set Connection pool

--- 10_BackEnd/src/Listeners/MyListener.java
+++ 10_BackEnd/src/Listeners/MyListener.java
@@ -1,5 +1,8 @@
 package Listeners;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -9,6 +12,17 @@ public class MyListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         System.out.println("context Initialized");
+
+        BasicDataSource pool=new BasicDataSource();
+        pool.setDriverClassName("com.mysql.jdbc.Driver");
+        pool.setUrl("jdbc:mysql://localhost:3306/pos_system");
+        pool.setUsername("root");
+        pool.setPassword("1234");
+        pool.setMaxTotal(5);
+        pool.setInitialSize(5);
+
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.setAttribute("pool",pool);
     }
 
     @Override
