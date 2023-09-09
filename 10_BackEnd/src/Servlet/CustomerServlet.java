6137c87caa2f2d13f45a39d300434d33e69ce60a
Udith_Dev_Wee <daveudith@gmail.com>
2023-09-09 11:36:23 +0530

    create frontend and backend seperate module for practice

---
+++ 10_BackEnd/src/Servlet/CustomerServlet.java
@@ -0,0 +1,21 @@
+package Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("DO Get Method INVOKED");
+
+        PrintWriter writer = resp.getWriter();
+        writer.write("Response Ok");
+    }
+}
