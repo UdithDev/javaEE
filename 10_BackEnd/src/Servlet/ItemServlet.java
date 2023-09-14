d1b5e542758f6e95d645b05f074560aa4303ad10
Udith_Dev_Wee <daveudith@gmail.com>
2023-09-14 18:50:59 +0530

    refactor get method

--- 10_BackEnd/src/Servlet/ItemServlet.java
+++ 10_BackEnd/src/Servlet/ItemServlet.java
@@ -1,5 +1,11 @@
 package Servlet;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/item")
 public class ItemServlet extends HttpServlet {
@@ -21,7 +31,49 @@ public class ItemServlet extends HttpServlet {
         resp.addHeader("Access-Control-Allow-Origin", "*");
 
         PrintWriter writer = resp.getWriter();
-        writer.write("Response ekak awaa");
+
+        ServletContext servletContext = req.getServletContext();
+        BasicDataSource pool = (BasicDataSource) servletContext.getAttribute("pool");
+
+
+        try (Connection connection = pool.getConnection();){
+            PreparedStatement preparedStatement = connection.prepareStatement("Select  *  from item");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            JsonArrayBuilder allItems = Json.createArrayBuilder();
+            while (resultSet.next()) {
+                String itemID = resultSet.getString(1);
+                String itemName = resultSet.getString(2);
+                String itemQty = resultSet.getString(3);
+                String itemPrice = resultSet.getString(4);
+
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("code",itemID);
+                objectBuilder.add("description",itemName);
+                objectBuilder.add("itemQty",itemQty);
+                objectBuilder.add("unitPrice",itemPrice);
+
+                System.out.println(itemID+" "+itemName+" "+itemQty+" "+itemPrice);
+
+                allItems.add(objectBuilder.build());
+
+                JsonObjectBuilder response = Json.createObjectBuilder();
+                response.add("status", 200);
+                response.add("message", "Done");
+                response.add("data", allItems.build());
+
+                writer.print(response.build());
+            }
+        } catch (SQLException e) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            resp.setStatus(200);
+            response.add("status", 400);
+            response.add("message", "SQL ERROR");
+            response.add("data", e.getLocalizedMessage());
+
+            writer.print(response.build());
+        }
+
 
     }
 }
--- 11_Front_End/controllers/ItemController.js
+++ 11_Front_End/controllers/ItemController.js
@@ -2,10 +2,16 @@ $("#itemGetAll").click(function () {
     $.ajax({
         url: "http://localhost:8081/10_BackEnd/item",
         method: "GET",
+        dataType:'json',
 
 
         success: function (resp) {
-            console.log(resp);
+            console.log(resp.data);
+            for (const i of resp.data) {
+                 let row=`<tr><td>${i.code}</td><td>${i.description}</td><td>${i.itemQty}</td><td>${i.unitPrice}</td></tr>`
+                 $("#tblItem").append(row);
+            }
+
         },
         error: function (xhr) {
             console.log(xhr);
